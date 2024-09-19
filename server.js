3d1a1a20a52f3dc771199e9138fb12e5aeff0b92
kadas <kaustav.das@here.com>
2024-09-19 14:20:56 +0530

    json server

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
+  }
+  next();
+});
 server.use(router);
 
 const port = process.env.PORT || 3000;
-
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
